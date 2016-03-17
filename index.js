d5aa024615168aa5360e08454b17037fbdc042d5
JttWest <johnson.h.2010@hotmail.com>
2016-03-16 23:08:05 -0700

    fixed script

--- index.js
+++ index.js
@@ -15,21 +15,21 @@ var message_queue = [];
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
 app.get('/retrieveData', function (req, res) {
 	if (message_queue)
-		res.end(message_queue.pop);
+		res.end(message_queue.pop());
 	else
 		res.end("No message in queue.");
 
 }))
 
 app.post('/sendData', function (req, res) {
-   var user_name = push(req.body);
+   var user_name = message_queue.push(req.body);
 }))
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
