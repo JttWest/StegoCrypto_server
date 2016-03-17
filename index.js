830b936a22643c04f2f9a815ea13b83710805ed3
JttWest <johnson.h.2010@hotmail.com>
2016-03-16 23:05:09 -0700

    added basic restful api

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+
+var message_queue = [];
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -12,6 +18,18 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+
+app.get('/retrieveData', function (req, res) {
+	if (message_queue)
+		res.end(message_queue.pop);
+	else
+		res.end("No message in queue.");
+
+}))
+
+app.post('/sendData', function (req, res) {
+   var user_name = push(req.body);
+}))
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
